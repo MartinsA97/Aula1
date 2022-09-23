bf8be7dde1057b5d43a1585935b7b2d07a4ee773
antoniomartins <martinsantonio@ipvc.pt>
2022-09-23 09:42:06 +0100

    Update 1

--- app/src/main/java/com/example/aula1/MainActivity.kt
+++ app/src/main/java/com/example/aula1/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // o Eduardo é feio
+        // o pedro nao gosta da mulher
+
+
     }
 }
